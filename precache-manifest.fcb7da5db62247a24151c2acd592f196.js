2be5e51c04f57f672ffecedecfd4022cb9483c9d
Copter1998 <panutat10@gmail.com>
2020-07-23 12:38:49 +0700

    Updates

---
+++ precache-manifest.fcb7da5db62247a24151c2acd592f196.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00236bf039cd3d5cf4c50afccdf6ca0b",
+    "url": "/basicreact2/index.html"
+  },
+  {
+    "revision": "f8754d62728cdaaec2e5",
+    "url": "/basicreact2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4c3004c35be456702f14",
+    "url": "/basicreact2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f8754d62728cdaaec2e5",
+    "url": "/basicreact2/static/js/2.f48fbbcf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/basicreact2/static/js/2.f48fbbcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3004c35be456702f14",
+    "url": "/basicreact2/static/js/main.84b451a6.chunk.js"
+  },
+  {
+    "revision": "8592c13af3c27f29298c",
+    "url": "/basicreact2/static/js/runtime-main.c28fb009.js"
+  }
+]);
